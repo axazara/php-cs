6967f285576728fc7eddbcfc06ce0157e4861fb9
Elias Elimah <elias.elimah@axazara.com>
2023-08-19 15:55:08 +0100

    Setup package and add PHP CS rule

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+use function array_merge;
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+function styles(Finder $finder, array $rules = []): Config
+{
+    $rules = array_merge(require __DIR__.'/rules.php', $rules);
+
+    return Config::create()
+        ->setFinder($finder)
+        ->setRiskyAllowed(true)
+        ->setRules($rules);
+}
