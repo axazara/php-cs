6236c8c379561cc725c4e6c056ab09a1e1eae555
Elias Elimah <9350271-EliasElimah@users.noreply.gitlab.com>
2024-07-14 18:51:17 +0100

    Fix code style

--- tests/FinderTest.php
+++ tests/FinderTest.php
@@ -6,7 +6,6 @@ use AxaZara\CS\Finder;
 use PhpCsFixer\Finder as PhpCsFixerFinder;
 use PHPUnit\Framework\TestCase;
 
-
 class FinderTest extends TestCase
 {
     public function test_method_return_array(): void
